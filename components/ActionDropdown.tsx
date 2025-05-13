b71ee50a3119305aa090a30c484bfd3e4b7f17dc
Muhamad Hanif Hafizhan <muhamadhanifhafizhan@gmail.com>
2025-05-13 08:05:08 +0700

    add : added file details modal view

--- components/ActionDropdown.tsx
+++ components/ActionDropdown.tsx
@@ -26,6 +26,7 @@ import { Button } from "./ui/button";
 import { renameFile } from "@/lib/actions/file.actions";
 import { usePathname } from "next/navigation";
 import { toast } from "@/hooks/use-toast";
+import { FileDetails } from "./ActionModalContent";
 
 const ActionDropdown = ({ file }: { file: Models.Document }) => {
   const [isModalOpen, setIsModalOpen] = useState(false);
@@ -84,6 +85,7 @@ const ActionDropdown = ({ file }: { file: Models.Document }) => {
               onChange={(e) => setName(e.target.value)}
             />
           )}
+          {value === "details" && <FileDetails file={file} />}
         </DialogHeader>
         {["rename", "delete", "share"].includes(value) && (
           <DialogFooter className="flex flex-col gap-3 md:flex-row ">
